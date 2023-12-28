0fae005e0c11a7f9855307348076d19f906d1976
mumiroMurorin <murorin99@gmail.com>
2023-12-29 05:55:59 +0900

    ようわからんけど一回プッシュ[注意]

--- Assets/C#/GameCtrl.cs
+++ Assets/C#/GameCtrl.cs
@@ -7,64 +7,43 @@ public class GameCtrl : MonoBehaviour
     [Header("ファイル名(仮)")]
     [SerializeField] private string file_name;
 
-    [Header("楽曲スタートアクションボックス")]
-    [SerializeField] private GameObject musicStart_box;
     [SerializeField] private ScoreCtrl scoreCtrl;
     [SerializeField] private SoundCtrl soundCtrl;
-    [SerializeField] private UICtrl uiCtrl;
 
-    //ステップ系
-    private bool isFileGettingReady;
-    private bool isDataGettingReady;
+    private bool isGettingReady;
     private bool isPlayingGame;
-    
-    //トリガー系
-    private bool isFileLoadTrigger;
-    private bool isDataPreparationTrigger;
-    private bool isGameStartTrigger;
 
     //準備系
     private bool isReadyComp;
 
+
     void Start()
     {
-        SetFileTrigger();   //仮
-        SetDataTrigger();   //仮
+        
     }
 
     void Update()
     {
         //ファイル準備
-        if (isFileLoadTrigger) 
-        { SetFileStep(); }
-        //データ準備(プレイ準備)
-        else if(isFileGettingReady && scoreCtrl.IsReturnReadDataComp() && isDataPreparationTrigger)
-        { SetDataStep(); }
+        if (!isGettingReady) { SetFileTrigger(); }
         //ファイル準備完了？
-        else if (!isReadyComp && isDataGettingReady && scoreCtrl.IsReturnScoreReady() && soundCtrl.IsReturnLoadComp())
-        { isReadyComp = true; }
-        //楽曲プレイ
-        else if (!isPlayingGame && isReadyComp && isGameStartTrigger)
-        { StartStep(); }
-        //楽曲終了
-        else if(isPlayingGame && !scoreCtrl.IsReturnPlaying()) 
-        { FinishGameStep(); }
+        else if (!isReadyComp && scoreCtrl.IsReturnScoreReady() && soundCtrl.IsReturnLoadComp()) 
+        { 
+            isReadyComp = true;
+        }
     }
 
     //初期化
     private void Init()
     {
-        isFileGettingReady = false;
-        isDataGettingReady = false;
+        isGettingReady = false;
         isReadyComp = false;
         isPlayingGame = false;
     }
 
-    //楽曲決定(初選曲)時処理
-    //(楽曲、譜面、演出他の準備トリガー)
-    private void SetFileStep()
+    //楽曲、譜面、演出他の準備トリガー
+    private void SetFileTrigger()
     {
-        Init();
         //譜面系のファイルセット
         scoreCtrl.Init();
         scoreCtrl.ReadStart(file_name);
@@ -72,70 +51,16 @@ public class GameCtrl : MonoBehaviour
         soundCtrl.Init();
         soundCtrl.ReadStart(file_name);
 
-        isFileGettingReady = true;
-        isDataGettingReady = false;
-        isPlayingGame = false;
-
-        isFileLoadTrigger = false;
+        isGettingReady = true;
     }
 
-    //楽曲スタート、リスタート時処理
-    //(譜面事前生成ほか)
-    private void SetDataStep()
-    {
-        //譜面の初期化
-        scoreCtrl.Init_Start();
-        //楽曲の初期化
-        soundCtrl.Init_Start();
-        //UIの初期化
-        uiCtrl.Init_Start();
-
-        musicStart_box.SetActive(true);
-
-        isDataGettingReady = true;
-        isPlayingGame = false;
-
-        isDataPreparationTrigger = false;
-    }
-
-    //楽曲(ゲーム)スタートトリガー(ボタンかなにか)
-    private void StartStep()
+    //ゲームスタートトリガー(ボタンかなにか)
+    public void StartTrigger()
     {
         if (!isReadyComp) { return; }
         scoreCtrl.GameStart();  //譜面開始
         soundCtrl.PlayMusic();  //楽曲再生
-
-        musicStart_box.SetActive(false);
-
         isPlayingGame = true;
-        isGameStartTrigger = false;
     }
 
-    //楽曲(ゲーム)終了処理
-    private void FinishGameStep()
-    {
-        isPlayingGame = false;
-        soundCtrl.StopMusic();      //楽曲停止(フェードアウト)
-        uiCtrl.AdventResultUI();    //リザルトUI出現
-    }
-
-    //--------------------トリガー系--------------------
-
-    //ファイル読み込みトリガー
-    public void SetFileTrigger()
-    {
-        isFileLoadTrigger = true;
-    }
-
-    //データ準備トリガー
-    public void SetDataTrigger()
-    {
-        isDataPreparationTrigger = true;
-    }
-
-    //楽曲プレイトリガー
-    public void PlayGameTrigger()
-    {
-        isGameStartTrigger = true;
-    }
 }
